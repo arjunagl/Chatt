700e656decdbe10b5d2c100e6116b30fe04b36f4
Arjuna Marambe <amarambe@seek.com.au>
2019-11-30 08:58:56 +1100

    dev

--- src/components/friends/search/FriendSearchComponent.tsx
+++ src/components/friends/search/FriendSearchComponent.tsx
@@ -1,6 +1,8 @@
 import { connect } from 'react-redux';
-import React, { ChangeEventHandler, useState } from 'react';
+import React, { ChangeEventHandler, useState, useEffect } from 'react';
 import * as FriendSearchComponentStyles from './FriendSearchComponentStyles';
+import { debounceTime } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 const friendComponent = React.memo(() => {
   const [formData, setSearchTerm] = useState({
@@ -14,8 +16,22 @@ const friendComponent = React.memo(() => {
       ...{ searchTerm: { valid: true, value: newValue } }
     };
     setSearchTerm(formDataToSet);
-    // subject.next(formDataToSet);
+    subject.next(formDataToSet);
   };
+
+  const subject = new Subject();
+  const formInput$ = subject.pipe(debounceTime(5000));
+  // Using effects to setup the subscription and unsubscribe from the subscription
+  useEffect(() => {
+    formInput$.subscribe(inputData => {
+      console.log(`dispatching the friends search = ${JSON.stringify(inputData, null, 2)}`);
+    });
+
+    return function cleanup() {
+      // @ts-ignore
+      formInput$.unsubscribe();
+    };
+  });
 
   return (
     <FriendSearchComponentStyles.containerBlock>
