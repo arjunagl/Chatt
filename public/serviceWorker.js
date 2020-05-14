64cb0bd6bb071e0e6c7f95ecdf4fa1a2491d42b0
Arjuna Marambe <amarambe@seek.com.au>
2020-05-15 08:23:10 +1000

    d

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,26 +1,23 @@
-const establishConnectionToServer = () => {
-  const swUrl = CHATT_SERVER;
-  console.log(`swUrl = ${swUrl}`);
-};
+// const establishConnectionToServer = () => {};
 
 /* eslint-disable no-undef */
 /* eslint-disable no-restricted-globals */
-self.addEventListener('install', function installEvent(event) {
+self.addEventListener('activate', function installEvent(event) {
   console.log(`service worker is now active`);
 });
 
-self.addEventListener('fetch', function fetchEvent(event) {
-  event.respondWith(
-    caches.open('chatt-dynamic').then(function cacheOpen(cache) {
-      return cache.match(event.request).then(function cacheOpenResponse(response) {
-        return (
-          response ||
-          fetch(event.request).then(function fetchRequestEvent(frResponse) {
-            cache.put(event.request, frResponse.clone());
-            return response;
-          })
-        );
-      });
-    })
-  );
-});
+// self.addEventListener('fetch', function fetchEvent(event) {
+//   event.respondWith(
+//     caches.open('chatt-dynamic').then(function cacheOpen(cache) {
+//       return cache.match(event.request).then(function cacheOpenResponse(response) {
+//         return (
+//           response ||
+//           fetch(event.request).then(function fetchRequestEvent(frResponse) {
+//             cache.put(event.request, frResponse.clone());
+//             return response;
+//           })
+//         );
+//       });
+//     })
+//   );
+// });
--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -1,4 +1,4 @@
-const establishConnectionToServer = () => {};
+// const establishConnectionToServer = () => {};
 
 /* eslint-disable no-undef */
 /* eslint-disable no-restricted-globals */
@@ -6,18 +6,18 @@ self.addEventListener('activate', function installEvent(event) {
   console.log(`service worker is now active`);
 });
 
-self.addEventListener('fetch', function fetchEvent(event) {
-  event.respondWith(
-    caches.open('chatt-dynamic').then(function cacheOpen(cache) {
-      return cache.match(event.request).then(function cacheOpenResponse(response) {
-        return (
-          response ||
-          fetch(event.request).then(function fetchRequestEvent(frResponse) {
-            cache.put(event.request, frResponse.clone());
-            return response;
-          })
-        );
-      });
-    })
-  );
-});
+// self.addEventListener('fetch', function fetchEvent(event) {
+//   event.respondWith(
+//     caches.open('chatt-dynamic').then(function cacheOpen(cache) {
+//       return cache.match(event.request).then(function cacheOpenResponse(response) {
+//         return (
+//           response ||
+//           fetch(event.request).then(function fetchRequestEvent(frResponse) {
+//             cache.put(event.request, frResponse.clone());
+//             return response;
+//           })
+//         );
+//       });
+//     })
+//   );
+// });
